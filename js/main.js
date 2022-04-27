49ef95a380d0ea9e8cc1ba06ac80b3d2898ad5e7
Gaki13 <gustavoantoniolopez1311@gmail.com>
2022-04-27 16:54:47 +0200

    creazione delle variabili "domanda"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-//const nome = prompt ("Come ti chiami");
+//chiedo il nome
+const name = prompt ("Come si chiama il tuo nome?");
+//chiedo il congnome
+const lastName = prompt("Come si chiama il tuo cognome?");
+//chiedo il colore preferito
+const Color = prompt("Qual è il tuo colore preferito?")
+//creo la password aggiungendo 21
